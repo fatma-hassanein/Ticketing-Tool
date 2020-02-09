dbc765378380b657e3b2c3d9024c179a23436e65
Fatma Mohamed Hassanein <fatma.hassanein129@gmail.com>
2020-02-09 12:13:07 +0200

    Update webpack to read images

--- webpack.prod.js
+++ webpack.prod.js
@@ -26,6 +26,17 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                    },
+                    },
+                ],
             }
         ]
     },
