c4c2a171847bae7874d75be2333ea6b93c3b6d16
Fatma Mohamed Hassanein <fatma.hassanein129@gmail.com>
2020-02-16 09:39:04 +0200

    Update index.js to add TicketsHistory function

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,9 @@
 import './styles/style.scss'
 import logo from './assets/images/logo.png'
+import { getTicketsHistory } from './js/app.js'
+
 document.getElementById('logo').src = logo;
+
+
+
+export { getTicketsHistory }
