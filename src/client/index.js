8bd6d9cc76ae9d9d101b2e63e2dd5dbbf5a23315
Ahmed Magdy <madimagdy@gmail.com>
2020-02-23 12:20:03 +0200

    Update table structure and styling

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,4 @@
 import './styles/style.scss';
-import logo from './assets/images/logo.png';
 import { getTicketsHistory, getUserInputs } from './js/form';
-
-document.getElementById('logo').src = logo;
 
 export { getTicketsHistory, getUserInputs };
